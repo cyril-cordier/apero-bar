a44707cac140a408d5ad525694d2be552fde828d
Cyril CORDIER <65895103+cyril-cordier@users.noreply.github.com>
2021-12-31 18:45:54 +0100

    url local to url online

--- src/apis/BottleFinder.js
+++ src/apis/BottleFinder.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL:"https://api-apero-bar.herokuapp.com/api",
-    baseURL:"http://localhost:8080/api",
+    baseURL:"https://api-apero-bar.herokuapp.com/api",
+    // baseURL:"http://localhost:8080/api",
 })
